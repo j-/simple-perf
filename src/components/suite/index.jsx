5915bafb4061e00788370efd39f4998d8082b188
j- <j@skeoh.com>
2016-05-18 20:48:08 +1000

    Add "Run suite" button

--- src/components/suite/index.jsx
+++ src/components/suite/index.jsx
@@ -3,15 +3,26 @@ import SubjectContainer from '../subject/container';
 import SubjectList from '../subject-list';
 
 export default class Suite extends React.Component {
+	runSuite () {
+
+	}
+
 	render () {
+		const {
+			props,
+			runSuite,
+		} = this;
 		const {
 			list,
 			addItem,
-		} = this.props;
+		} = props;
 		return <div>
 			<SubjectList list={ list } Item={ SubjectContainer } />
 			<button onClick={ addItem }>
 				Add new item
+			</button>
+			<button onClick={ runSuite }>
+				Run suite
 			</button>
 		</div>;
 	}
