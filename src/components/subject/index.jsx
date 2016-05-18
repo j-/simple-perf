57b3f2e698bbcfa9c7cbbc2f743ca60d057d050a
j- <j@skeoh.com>
2016-05-18 20:16:50 +1000

    Add action buttons to subject component

--- src/components/subject/index.jsx
+++ src/components/subject/index.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 export default class Subject extends React.Component {
 	render () {
-		const { item } = this.props;
+		const {
+			item,
+			moveUp,
+			moveDown,
+			remove,
+		} = this.props;
 		const { id } = item;
-		return <div>{ id }</div>;
+		return <div>
+			<span>ID: { id }</span>
+			<div class="subject-actions">
+				<button onClick={ () => moveUp(item) }>Move up</button>
+				<button onClick={ () => moveDown(item) }>Move down</button>
+				<button onClick={ () => remove(item) }>Delete</button>
+			</div>
+		</div>;
 	}
 }
