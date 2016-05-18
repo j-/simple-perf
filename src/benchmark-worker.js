30dccd8abd73ecc9e8e854534be4fe8e9028b8e5
j- <j@skeoh.com>
2016-05-18 21:16:33 +1000

    Update benchmark worker

--- src/benchmark-worker.js
+++ src/benchmark-worker.js
@@ -3,27 +3,23 @@ import register from 'promise-worker/register';
 
 console.log(Benchmark.platform);
 
-const suite = new Benchmark.Suite();
-
-suite.on('cycle', ({ target }) => {
-	console.log(String(target));
-});
-
 register((message) => {
 	if (message.type !== 'test') {
 		return;
 	}
 
+	const suite = new Benchmark.Suite();
+
+	suite.on('cycle', ({ target }) => {
+		console.log(String(target));
+	});
+
 	suite.reset();
 
-	message.tests.forEach(({ name, source }, i) => {
-		const fn = source;
-		suite.add(name, fn, {
-			onStart: ({ target }) => {
-				if (i === 1) {
-					target.abort();
-				}
-			},
+	message.tests.forEach((test) => {
+		suite.add({
+			name: test.id,
+			fn: test.source,
 		});
 	});
 
