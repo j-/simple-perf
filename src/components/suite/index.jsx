c842c701e1a1a8639a6c7166d865ce07f3b2648c
j- <j@skeoh.com>
2016-05-18 21:17:42 +1000

    Run benchmark suite when clicking button

--- src/components/suite/index.jsx
+++ src/components/suite/index.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 import SubjectContainer from '../subject/container';
 import SubjectList from '../subject-list';
 
+import store from '../../store';
+
+import PromiseWorker from 'promise-worker';
+const worker = new Worker('benchmark-worker.js');
+const promiseWorker = new PromiseWorker(worker);
+
 export default class Suite extends React.Component {
 	runSuite () {
-
+		const tests = store.getState();
+		promiseWorker.postMessage({
+			type: 'test',
+			tests,
+		});
 	}
 
 	render () {
 		const {
-			props,
-			runSuite,
-		} = this;
-		const {
 			list,
 			addItem,
-		} = props;
+		} = this.props;
+		const runSuite = () => this.runSuite();
 		return <div>
 			<SubjectList list={ list } Item={ SubjectContainer } />
 			<button onClick={ addItem }>
