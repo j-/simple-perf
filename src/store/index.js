ed2c444466fb6da75d8b70b568b288881c18b780
j- <j@skeoh.com>
2016-05-18 21:38:23 +1000

    Sync store to local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,16 @@
 import { createStore } from 'redux';
 import reducer from './subject-list/reducer';
 
-export default createStore(reducer);
+const LOCAL_STORAGE_KEY = 'simple-perf-store';
+const stringified = localStorage.getItem(LOCAL_STORAGE_KEY);
+const DEFAULT_VALUE = JSON.parse(stringified) || [];
+
+const store = createStore(reducer, DEFAULT_VALUE);
+
+store.subscribe(() => {
+	const state = store.getState();
+	const stringified = JSON.stringify(state);
+	localStorage.setItem(LOCAL_STORAGE_KEY, stringified);
+});
+
+export default store;
