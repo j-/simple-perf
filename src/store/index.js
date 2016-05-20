1cd79158e301bcff1f117cf0f131164217104a12
j- <j@skeoh.com>
2016-05-20 16:28:05 +1000

    Generate a default state of two test subjects

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,24 @@
 import { createStore } from 'redux';
+import uuid from 'node-uuid';
 import reducer from './subject-list/reducer';
+
+/**
+ * Creates a default state for the store. By default, a suite has two test
+ *   cases. Each test case has a random ID and no source.
+ * @return {Subject[]} List of test subjects
+ */
+function makeDefaultState () {
+	return [
+		{ id: uuid(), source: null },
+		{ id: uuid(), source: null },
+	];
+}
 
 const LOCAL_STORAGE_KEY = 'simple-perf-store';
 const stringified = localStorage.getItem(LOCAL_STORAGE_KEY);
-const DEFAULT_VALUE = JSON.parse(stringified) || [];
+const LOCAL_STORAGE_STATE = JSON.parse(stringified) || makeDefaultState();
 
-const store = createStore(reducer, DEFAULT_VALUE);
+const store = createStore(reducer, LOCAL_STORAGE_STATE);
 
 store.subscribe(() => {
 	const state = store.getState();
