2911895b6314d94eea4310918d3618a17073cdf2
j- <j@skeoh.com>
2016-05-20 16:33:50 +1000

    Persist default state to local storage after creation

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import { createStore } from 'redux';
 import uuid from 'node-uuid';
 import reducer from './subject-list/reducer';
 
+const LOCAL_STORAGE_KEY = 'simple-perf-store';
+
 /**
  * Creates a default state for the store. By default, a suite has two test
  *   cases. Each test case has a random ID and no source.
@@ -14,16 +16,43 @@ function makeDefaultState () {
 	];
 }
 
-const LOCAL_STORAGE_KEY = 'simple-perf-store';
-const stringified = localStorage.getItem(LOCAL_STORAGE_KEY);
-const LOCAL_STORAGE_STATE = JSON.parse(stringified) || makeDefaultState();
+/**
+ * Fetches previously saved local storage state.
+ * @return {?Subject[]} List of test subjects. Can be null if never saved.
+ */
+function loadLocalStorageState () {
+	const stringified = localStorage.getItem(LOCAL_STORAGE_KEY);
+	const parsed = JSON.parse(stringified);
+	return parsed;
+}
 
-const store = createStore(reducer, LOCAL_STORAGE_STATE);
+/**
+ * Saves a given state to local storage.
+ * @param {Subject[]} state Current state to save to local storage
+ */
+function saveLocalStorageState (state) {
+	const stringified = JSON.stringify(state);
+	localStorage.setItem(LOCAL_STORAGE_KEY, stringified);
+}
+
+const localStorageState = loadLocalStorageState();
+let preloadState;
+
+if (localStorageState) {
+	// Local storage state exists
+	preloadState = localStorageState;
+} else {
+	// Create new state tree
+	preloadState = makeDefaultState();
+	// Persist this new state tree
+	saveLocalStorageState(preloadState);
+}
+
+const store = createStore(reducer, preloadState);
 
 store.subscribe(() => {
 	const state = store.getState();
-	const stringified = JSON.stringify(state);
-	localStorage.setItem(LOCAL_STORAGE_KEY, stringified);
+	saveLocalStorageState(state);
 });
 
 export default store;
