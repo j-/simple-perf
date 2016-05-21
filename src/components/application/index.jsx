833aff8f82230f0e1f057616b6ffef1f3c83bf03
j- <j@skeoh.com>
2016-05-21 23:55:05 +1000

    Add page header to application component

--- src/components/application/index.jsx
+++ src/components/application/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { PageHeader } from 'react-bootstrap';
 import PageIndex from '../page-index';
 
 export default class Application extends React.Component {
 	render () {
 		return <div className="container">
+			<PageHeader>Simple Perf</PageHeader>
 			<PageIndex />
 		</div>;
 	}
