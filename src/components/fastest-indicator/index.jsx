723a0deff07d1d039487950915f9eef72a748d2f
j- <j@skeoh.com>
2016-05-21 23:23:11 +1000

    Add class name to fastest indicator component

--- src/components/fastest-indicator/index.jsx
+++ src/components/fastest-indicator/index.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 export default class FastestIndicator extends React.Component {
 	render () {
 		// Very simple for now
-		return <div>Fastest</div>;
+		return <div className="fastest">Fastest</div>;
 	}
 }
