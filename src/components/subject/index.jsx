ef881ea00ec676151172a8eb4e999ec338c205d4
j- <j@skeoh.com>
2016-05-21 23:54:09 +1000

    Add button groups to subject component

--- src/components/subject/index.jsx
+++ src/components/subject/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ButtonGroup, Button } from 'react-bootstrap';
 import CodeMirror from 'react-codemirror';
 import editorOptions from './editor-options';
 import SubjectStatus from '../subject-status';
@@ -23,21 +24,23 @@ export default class Subject extends React.Component {
 			onCancel,
 		} = this.props;
 		const fastestIndicator = isFastest ? <FastestIndicator /> : null;
-		const skipButton = canSkip ? <button onClick={ onSkip }>Skip test</button> : null;
-		const cancelButton = canCancel ? <button onClick={ onCancel }>Cancel test</button> : null;
+		const skipButton = canSkip ? <Button onClick={ onSkip }>Skip test</Button> : null;
+		const cancelButton = canCancel ? <Button onClick={ onCancel }>Cancel test</Button> : null;
 		return <div>
 			<CodeMirror
 				value={ source }
 				onChange={ onUpdateSource }
 				options={ editorOptions }
 			/>
-			<div className="subject-actions">
-				<button onClick={ onMoveUp } disabled={ !canMoveUp }>Move up</button>
-				<button onClick={ onMoveDown } disabled={ !canMoveDown }>Move down</button>
-				<button onClick={ onRemove }>Delete</button>
+			<ButtonGroup>
+				<Button onClick={ onMoveUp } disabled={ !canMoveUp }>Move up</Button>
+				<Button onClick={ onMoveDown } disabled={ !canMoveDown }>Move down</Button>
+				<Button onClick={ onRemove }>Delete</Button>
+			</ButtonGroup>
+			<ButtonGroup>
 				{ skipButton }
 				{ cancelButton }
-			</div>
+			</ButtonGroup>
 			<SubjectStatus status={ status } />
 			{ fastestIndicator }
 		</div>;
