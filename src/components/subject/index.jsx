169f6549810ed9f165923204ad21d49ab5c8637a
j- <j@skeoh.com>
2016-05-22 15:43:31 +1000

    Change test subject appearance

--- src/components/subject/index.jsx
+++ src/components/subject/index.jsx
@@ -4,6 +4,7 @@ import {
 	ButtonGroup,
 	Button,
 	Panel,
+	Glyphicon,
 } from 'react-bootstrap';
 import CodeMirror from 'react-codemirror';
 import editorOptions from './editor-options';
@@ -19,6 +20,7 @@ export default class Subject extends React.Component {
 			status,
 			isFastest,
 			stats,
+			index,
 			canMoveUp,
 			canMoveDown,
 			canSkip,
@@ -35,25 +37,37 @@ export default class Subject extends React.Component {
 		const cancelButton = canCancel ? <Button onClick={ onCancel }>Cancel test</Button> : null;
 		const testStats = stats !== null ? <TestStats { ...stats } /> : null;
 		return <div className="subject">
+			<div className="clearfix">
+				<big className="subject-index">
+					Test { index + 1 }
+				</big>
+				<div className="pull-right">
+					<ButtonGroup>
+						<Button bsStyle="link" onClick={ onMoveUp } disabled={ !canMoveUp }>
+							<Glyphicon glyph="chevron-up" />
+						</Button>
+						<Button bsStyle="link" onClick={ onMoveDown } disabled={ !canMoveDown }>
+							<Glyphicon glyph="chevron-down" />
+						</Button>
+						<Button bsStyle="link" onClick={ onRemove }>
+							<Glyphicon glyph="remove" />
+						</Button>
+					</ButtonGroup>
+				</div>
+			</div>
 			<CodeMirror
 				value={ source }
 				onChange={ onUpdateSource }
 				options={ editorOptions }
 			/>
 			<br />
-			<ButtonToolbar>
-				<ButtonGroup>
-					<Button onClick={ onMoveUp } disabled={ !canMoveUp }>Move up</Button>
-					<Button onClick={ onMoveDown } disabled={ !canMoveDown }>Move down</Button>
-					<Button onClick={ onRemove }>Delete</Button>
-				</ButtonGroup>
-				<ButtonGroup>
-					{ skipButton }
-					{ cancelButton }
-				</ButtonGroup>
-			</ButtonToolbar>
-			<br />
 			<Panel>
+				<ButtonToolbar className="pull-right">
+					<ButtonGroup>
+						{ skipButton }
+						{ cancelButton }
+					</ButtonGroup>
+				</ButtonToolbar>
 				{ fastestIndicator }
 				<SubjectStatus status={ status } />
 				{ testStats }
