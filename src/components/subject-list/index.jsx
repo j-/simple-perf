40f71c711ba9141b821a79456a4c04472ea15cfb
j- <j@skeoh.com>
2016-05-30 00:32:42 +1000

    Add prop types to subject list component

--- src/components/subject-list/index.jsx
+++ src/components/subject-list/index.jsx
@@ -1,7 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import SubjectListItem from '../subject-list-item';
 
 export default class SubjectList extends Component {
+	static propTypes = {
+		list: PropTypes.arrayOf(PropTypes.object).isRequired,
+		Item: PropTypes.any.isRequired,
+	}
+
 	render () {
 		const { list, Item } = this.props;
 		const items = list.map((item) => {
